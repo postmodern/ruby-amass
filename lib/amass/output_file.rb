61615f3b64d8688502f6d5194005061ec5fb48ab
Postmodern <postmodern.mod3@gmail.com>
2021-11-30 06:47:42 -0800

    Made Amass::OutputFile more like Masscan::OutputFile from ruby-masscan.

--- lib/amass/output_file.rb
+++ lib/amass/output_file.rb
@@ -9,21 +9,64 @@ module Amass
   #
   class OutputFile
 
+    # Mapping of formats to parsers.
     PARSERS = {
-      '.json' => Parsers::JSON,
-      '.txt'  => Parsers::TXT
+      :json => Parsers::JSON,
+      :txt  => Parsers::TXT
     }
 
+    # The path to the output file.
+    #
     # @return [String]
     attr_reader :path
+
+    # The format of the output file.
+    #
+    # @return [:json, :txt]
+    attr_reader :format
+
+    # The parser for the output file format.
+    #
+    # @return [Parsers::JSON, Parsers::TXT]
+    #
+    # @api private
+    attr_reader :parser
 
     #
     # @param [String] path
     #
-    def initialize(path)
+    # @param [:json, :txt] format
+    #
+    def initialize(path, format: self.class.infer_format(path))
       @path   = File.expand_path(path)
-      @parser = PARSERS.fetch(File.extname(@path)) do
+      @format = format
+
+      @parser = PARSERS.fetch(format) do
         raise(ArgumentError,"unrecognized file type: #{@path.inspect}")
+      end
+    end
+
+    # Mapping of file extensions to formats
+    FILE_FORMATS = {
+      '.json' => :json,
+      '.txt'  => :txt
+    }
+
+    #
+    # Infers the format from the output file's extension name.
+    #
+    # @param [String] path
+    #   The path to the output file.
+    #
+    # @return [:json, :txt]
+    #   The output format inferred from the file's extension name.
+    #
+    # @raise [ArgumentError]
+    #   The output format could not be inferred from the file's name.
+    #
+    def self.infer_format(path)
+      FILE_FORMATS.fetch(File.extname(path)) do
+        raise(ArgumentError,"could not infer format of #{path}")
       end
     end
 
